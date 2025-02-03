5c5ebebf6e41b472e512bc49ec4c6dba4c562478
Kesavan G <kesavannivesh272006@gmail.com>
2025-02-03 10:24:16 +0530

    My latest commits at 10:24 on 3/02/25

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,13 +4,16 @@ import { auth } from '../config/firebase';
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import Searchicon from '../assets/searchicon2.svg'
-import homeicon from '../assets/homeicon.svg'
-import chaticon from '../assets/chaticon.svg'
-import notificationicon from '../assets/notificationsicon.svg'
-import profileicon from '../assets/profileicon.svg'
-import logouticon from '../assets/logouticon.svg'
-import networkicon from '../assets/network.svg'
-function Navbar(){
+import { NavLink } from 'react-router-dom';
+
+
+import { GoHomeFill } from "react-icons/go";
+import { TiMessages } from "react-icons/ti";
+import { IoIosNotifications } from "react-icons/io";
+import { IoPersonSharp } from "react-icons/io5";
+import { MdLogout } from "react-icons/md";
+import { IoIosPeople } from "react-icons/io";
+function Navbar() {
     const navigate = useNavigate();
     const logout = async () => {
         try {
@@ -21,23 +24,33 @@ function Navbar(){
         }
     };
     console.log(Searchicon);
-    return(
-        <div className="bg-white py-7 navbar">
-            <nav className='flex items-center justify-between'>
-                <div className="left flex items-center md:text-6xl text-3xl ml-4 md:ml-10 gap-5">
-                    <Link to='/about'><img src={logo} className='logo md:block md:h-20 md:w-20 hidden rounded-full' /></Link>
-                    <Link to='/about'><h1 className='russo pt-3'>KOTINOS</h1></Link>
+    return (
+        <div className="bg-white border border-gray-100 border-x-0 border-t-0 w-full py-3">
+            <style>
+                {`
+                    .search-bar:focus{
+                        outline: none;
+                    }
+                `}
+            </style>
+            <nav className='flex items-center md:w-full md:justify-between'>
+                <div className="flex items-center md:gap-5 px-2 md:w-full">
+                    <Link to='/about'><img src={logo} className='md:block mx-2 h-12 w-12 hidden rounded-full' /></Link>
+                    {/* <Link to='/about'><h1 className='russo pt-3'>KOTINOS</h1></Link> */}
+                    <div className="search border border-gray-500 flex items-center w-[90%] md:w-[30%] rounded-full">
+                        <img src={Searchicon} alt="Search" className="m-2" />
+                        <input type="text" placeholder="Search" className="search-bar w-full md:w-full  text-lg rounded-full active:border-none md:py-2 md:pr-8" />
+                    </div>
                 </div>
-                <div style={{backgroundColor: 'rgb(234, 235, 227)', width: '461px', height: '63px', alignContent: 'center'}}>
-                    <img src={Searchicon} alt="Search" style={{paddingLeft: '10px', height: '40px', width: '40px', color: 'black', }}  />
-                </div>
-                <div className="ul">
-                    <ul className='flex mr-4 md:text-2xl text-l mt-4 md:pr-10 md:mt-5'>
-                        <Link to='/home' className='md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer'><img src={homeicon} alt="Home" /></Link>
-                        <Link to='/network' className='md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer'><img src={networkicon} alt="Network" /></Link>
-                        <Link to={'/messages'} className='md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer'><img src={chaticon} alt="Messages" /></Link>
-                        <Link to='/profile' className='md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer'><img src={profileicon} alt="Profile" /></Link>
-                        <button onClick={logout} className='md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer'><img src={logouticon} alt="Logout" /></button>
+
+                <div className="ul ">
+                    <ul className='flex items-center md:text-2xl gap-4 text-l pr-1 md:pr-10'>
+                    <NavLink to='/home' className={({ isActive }) => isActive ? 'active-link md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer' : 'md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer'}><GoHomeFill className='scale-[160%]' /></NavLink>
+                        <NavLink to='/messages' className={({ isActive }) => isActive ? 'active-link md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer' : 'md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer'}><TiMessages className='scale-[160%]' /></NavLink>
+                        <NavLink to='/notification' className={({ isActive }) => isActive ? 'active-link md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer' : 'md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer'}><IoIosNotifications className='scale-[160%]' /></NavLink>
+                        <NavLink to='/profile' className={({ isActive }) => isActive ? 'active-link md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer' : 'md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer'}><IoPersonSharp className='scale-[160%]' /></NavLink>
+                        <NavLink to='/network' className={({ isActive }) => isActive ? 'active-link md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer' : 'md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer'}><IoIosPeople className='scale-[160%]' /></NavLink>
+                        <button onClick={logout} className='md:ml-10 ml-4 russo hover:scale-110 transition-all cursor-pointer'><MdLogout className='scale-[160%]'/></button>
                     </ul>
                 </div>
             </nav>
