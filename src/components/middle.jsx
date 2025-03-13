bc61ad622ed0b9bd9b03bf8a8ab887a0f0401650
Aditya A <adityaarun2006@gmail.com>
2025-03-14 00:04:40 +0530

    Fixed several issues, Styled several pages.

--- src/components/middle.jsx
+++ src/components/middle.jsx
@@ -15,6 +15,7 @@ import likebutton from '../assets/likebutton.svg'
 import commenticon from '../assets/comment.svg'
 import { CiHeart } from "react-icons/ci";
 import liked from '../assets/liked.svg';
+import { BiCommentDetail } from "react-icons/bi";
 
 
 const Middle = ({ userData }) => {
@@ -351,14 +352,14 @@ const Middle = ({ userData }) => {
                                 
                             )}
                             
-                            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-evenly'}}>
+                            <div  className="gap-10" style={{display:'flex', flexDirection:'row',}}>
                                 <div className='mb-2 ml-5 mt-4 px-2 flex gap-1 items-center w-fit hover:bg-gradient-to-r hover:from-blue-900/20 hover:via-blue-700/20 hover:to-cyan-500/20 rounded-xl p-1'>
                                     <CiHeart  onClick={() => handlelikes(post.id, post.userId)} className='h-8 w-8  cursor-pointer scale-[80%]'/>
                                     {likeicon? <h1 className='text-blue-400'><strong>{post.likes}</strong></h1> : <h1><strong>{post.likes}</strong></h1>}
                                 </div>
-                                <div style={{marginLeft:'0%', display:'flex', flexDirection:'row', justifyContent:'start', alignItems:'center'}}>
-                                    <Button ><img src={commenticon} /></Button>
-                                    <h1 style={{fontSize:'25px'}}><strong>Comments</strong></h1>
+                                <div className='gap-3 mt-2' style={{marginLeft:'0%', display:'flex', flexDirection:'row', justifyContent:'start', alignItems:'center'}}>
+                                    <button ><BiCommentDetail className='scale-125' /></button>
+                                    <h1 className='font-bold'>0</h1>
                                 </div>
                             </div>
                         </div>
--- src/pages/Getstarted.jsx
+++ src/pages/Getstarted.jsx
@@ -1,45 +1,71 @@
+import gsap from 'gsap';
+import { useRef,useLayoutEffect } from 'react';
 import '../App.css';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 import Footer from '../components/footer';
 import Navbarout from '../components/getstartednavbar';
 import bg from '../assets/background.png';
+import test from '../test/test_bg1.jpg'
 
-function Getstarted(){
+function Getstarted() {
     const navigate = useNavigate();
 
     const handleclick = () => {
-        navigate("/signin");  
+        navigate("/signin");
     };
 
-    return(
+    const comp=useRef(null);
+    useLayoutEffect(() =>{
+      let ctx=gsap.context(()=>{
+        const t1=gsap.timeline()
+        t1.from("#main-text",{
+            opacity:0,
+            yPercent:30,
+            duration:0.8,
+            delay:0.3
+          }).from(["#sub-text","#sub-text-2","#get-started-button"],{
+            opacity:0,
+            y: "+=30",
+            stagger:0.2,
+          })
+        
+      },comp)
+
+      
+
+      return()=>ctx.revert()
+    },[])
+
+    return (
         <>
             <style>
                 {`
                     body{
-                        background-image: url(${bg});
+                        background-image: url(${test});
                         background-repeat: no-repeat;
                         background-size: cover;
                     }
                 `}
             </style>
             <Navbarout />
-            <div>
-            <div className="main-head flex items-center justify-center flex-col russo text-[28px] pt-40 pt-30 md:text-[64px] md:pt-32">
-            <div className=''>Empowering <span className='text-[#2BCEE0]'>Athletes</span>.</div>
-            <div>
-                <span className='text-[#2BCEE0]'>Elevating</span> Performance.
-            </div>
-            </div>
-            <div className="desc md:flex md:items-center md:justify-center m-3 md:m-8 md:mt-12 text-center text-[#848282] md:text-3xl">
-            <div className='md:w-[50%]'>Streamline team operations, manage schedules, and track performance all in one powerful platform.</div>
-            </div>
+            <div ref={comp}>
+                <div id='main-text' className="main-head flex items-center justify-center flex-col russo text-[28px] pt-20 md:text-[64px] md:pt-32">
+                    <div className=' text-[white]'>Empowering <span className='text-[#2BCEE0]'>Athletes</span>.</div>
+                    <div>
+                        <span className='text-[#2BCEE0]'>Elevating</span><span className='text-[white]'> Performance.</span>
+                    </div>
+                </div>
+                <div id='sub-text' className="desc md:flex md:items-center md:justify-center m-3 md:m-8 md:mt-12 text-center text-[white] md:text-3xl">
+                    <div className='md:w-[40%] text-xl'>Streamline team operations, manage schedules, and track performance all in one powerful platform.</div>
+                </div>
 
-            <div className="join-now-text flex flex-col justify-center items-center md:text-2xl mt-28 gap-5">
-            <div className=' text-[#787777]'>Start Your Sports Journey <span className='text-[#2BCEE0] hover:underline'>Today</span></div>
-            <div>
-                <button onClick={handleclick} className='rounded-[5px] px-5 py-2 bg-black text-white russo text-[17px] hover:scale-110 transition-all'>Get Started</button>
-            </div>
-            </div>
+                <div id='sub-text-2' className="join-now-text flex flex-col justify-center items-center md:text-2xl mt-28 gap-5">
+                    <div className=' text-[white] text-xl'>Start Your Sports Journey <span className='text-[#2BCEE0] hover:underline'>Today</span></div>
+                    
+                </div>
+                <div id="get-started-button" className='flex justify-center items-center mt-8'>
+                    <button onClick={handleclick} className='rounded-full px-5 py-2 bg-gradient-to-r from-blue-800 via-blue-700 to-blue-800 hover:bg-gradient-to-r hover:from-blue-800 hover:via-blue-600 hover:to-blue-800  text-white russo text-[17px] hover:scale-110 transition-all'>Get Started</button>
+                </div>
             </div>
             <hr className='line' />
             <Footer />
